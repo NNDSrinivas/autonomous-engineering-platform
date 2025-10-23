06c48e9c1cbd6c3d585f034fa83df07f1bbdca72
NNDSrinivas <srinivasn7779@gmail.com>
2025-10-22 22:59:17 -0400

    fix: use ApplicationManager.executeOnPooledThread, add input sanitization, use EDT-safe dialogs, optimize append with out.append()

--- extensions/jetbrains/src/main/kotlin/com/aep/ide/Status.kt
+++ extensions/jetbrains/src/main/kotlin/com/aep/ide/Status.kt
@@ -3,7 +3,19 @@ package com.aep.ide
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.WindowManager
 
+/**
+ * Utility for displaying messages in IntelliJ's status bar.
+ */
 object Status {
+  /**
+   * Shows a message in the status bar.
+   * 
+   * @param project The project instance
+   * @param message The message to display
+   * @param millis The intended duration to show the message (in milliseconds). Currently unused,
+   *               as IntelliJ auto-clears the status bar after a short period. This parameter is
+   *               retained for future extensibility or compatibility with other APIs.
+   */
   fun show(project: Project?, message: String, millis: Int = 8000) {
     val bar = project?.let { WindowManager.getInstance().getStatusBar(it) } ?: return
     bar.info = message
